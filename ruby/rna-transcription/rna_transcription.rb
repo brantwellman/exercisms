8648c304f8515bbfb7fe7a87e13f8b21172120e8
Brant Wellman <BrantWellman@gmail.com>
2016-05-13 11:40:59 -0600

    Add raise error method

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -5,14 +5,18 @@ class Complement
 
   def self.of_dna(strand)
     nucleotides = strand.split("")
-    dna_nucs = ['C', 'G', 'T', 'A']
     nucleotides.map do |nucleotide|
-      raise ArgumentError unless dna_nucs.include?(nucleotide)
+      raise_error(nucleotide)
       strand_converter[nucleotide]
     end.join
   end
 
   def self.strand_converter
     { 'C'=> 'G', 'G'=> 'C', 'T'=> 'A', 'A'=> 'U'}
+  end
+
+  def self.raise_error(nucleotide)
+    dna_nucs = ['C', 'G', 'T', 'A']
+    raise ArgumentError unless dna_nucs.include?(nucleotide)
   end
 end
