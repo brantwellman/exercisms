d439d019cca1da041527ea2c874d818d1a03e1fc
Brant Wellman <BrantWellman@gmail.com>
2016-06-01 13:44:27 -0600

    Ones passing

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,13 +1,21 @@
+require 'pry'
 
 class Fixnum
   VERSION = 1
-  
-  ARABIC_TO_ROMAN = {
-    1 => 'I'
-  }
 
   def to_roman
-    ARABIC_TO_ROMAN[self]
+    arabic_to_roman = {
+      1 => 'I'
+    }
+    roman_num = ''
+    arabic_num = self
+    arabic_to_roman.each do |arabic, roman|
+      while arabic_num > 0
+        roman_num = roman_num + arabic_to_roman[arabic]
+        arabic_num -= arabic
+      end
+    end
+    roman_num
   end
 
 end
