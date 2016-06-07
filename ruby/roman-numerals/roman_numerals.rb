3d4af407e76e9dbefc554c518001902d9132eeb3
Brant Wellman <BrantWellman@gmail.com>
2016-06-07 10:47:42 -0600

    Passing through CXLI

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -5,9 +5,15 @@ class Fixnum
 
   def to_roman
     arabic_to_roman = {
-      5 => 'V',
-      4 => 'IV',
-      1 => 'I'
+      100 => 'C',
+      90  => 'XC',
+      50  => 'L',
+      40  => 'XL',
+      10  => 'X',
+      9   => 'IX',
+      5   => 'V',
+      4   => 'IV',
+      1   => 'I'
     }
 
     roman_num = ''
--- ruby/roman-numerals/roman_numerals_test.rb
+++ ruby/roman-numerals/roman_numerals_test.rb
@@ -29,17 +29,14 @@ class RomanNumeralsTest < Minitest::Test
   end
 
   def test_6
-    skip
     assert_equal 'VI', 6.to_roman
   end
 
   def test_9
-    skip
     assert_equal 'IX', 9.to_roman
   end
 
   def test_27
-    skip
     assert_equal 'XXVII', 27.to_roman
   end
 
@@ -49,17 +46,14 @@ class RomanNumeralsTest < Minitest::Test
   end
 
   def test_59
-    skip
     assert_equal 'LIX', 59.to_roman
   end
 
   def test_93
-    skip
     assert_equal 'XCIII', 93.to_roman
   end
 
   def test_141
-    skip
     assert_equal 'CXLI', 141.to_roman
   end
 
