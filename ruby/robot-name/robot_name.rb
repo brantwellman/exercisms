ed5c791678887819e628170aef2b05044af43220
Brant Wellman <BrantWellman@gmail.com>
2016-06-09 23:19:45 -0600

    Last test not passing

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -1,10 +1,14 @@
 class Robot
 
   def name
-    @name = random_name
+    @name ||= random_name
   end
 
   def random_name
     ('A'..'Z').to_a.sample(2).join + rand.to_s[2..4]
+  end
+
+  def reset
+    @name = nil
   end
 end
--- ruby/robot-name/robot_name_test.rb
+++ ruby/robot-name/robot_name_test.rb
@@ -4,6 +4,7 @@
 gem 'minitest', '>= 5.0.0'
 require 'minitest/autorun'
 require_relative 'robot_name'
+require 'pry'
 
 class RobotTest < Minitest::Test
   DIFFERENT_ROBOT_NAME_SEED = 1234
@@ -21,20 +22,17 @@ class RobotTest < Minitest::Test
   end
 
   def test_name_sticks
-    skip
     robot = Robot.new
     robot.name
     assert_equal robot.name, robot.name
   end
 
   def test_different_robots_have_different_names
-    skip
     Kernel.srand DIFFERENT_ROBOT_NAME_SEED
     refute_equal Robot.new.name, Robot.new.name
   end
 
   def test_reset_name
-    skip
     Kernel.srand DIFFERENT_ROBOT_NAME_SEED
     robot = Robot.new
     name = robot.name
@@ -46,11 +44,11 @@ class RobotTest < Minitest::Test
   end
 
   def test_different_name_when_chosen_name_is_taken
-    skip
     Kernel.srand SAME_INITIAL_ROBOT_NAME_SEED
     name1 = Robot.new.name
     Kernel.srand SAME_INITIAL_ROBOT_NAME_SEED
     name2 = Robot.new.name
+    binding.pry
     refute_equal name1, name2
   end
 
