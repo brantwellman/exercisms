846be4da5212debf4af3ce90df5b255ae643afb9
Brant Wellman <BrantWellman@gmail.com>
2016-06-11 16:14:16 -0600

    Add duplicate_name? method

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -4,19 +4,14 @@ end
 
 class Robot
   @@names = []
-  attr_reader :name
 
   def name
     new_name = random_name
-    until @@names.include?(new_name) == false
+    while duplicate_name?(new_name)
       new_name = random_name
     end
     @@names << new_name
     @name ||= new_name
-  end
-
-  def unique_name?
-    @@names.include?(@name)
   end
 
   def reset
@@ -25,6 +20,10 @@ class Robot
   end
 
   private
+
+    def duplicate_name?(test_name)
+      @@names.include?(test_name)
+    end
 
     def random_name
       ('A'..'Z').to_a.sample(2).join + rand.to_s[2..4]
