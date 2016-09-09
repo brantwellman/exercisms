e4f53711ccde1ff703795fcc287289362bc7b1d4
Brant Wellman <BrantWellman@gmail.com>
2016-09-09 10:31:53 -0600

    All Grains tests passing

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,6 +1,24 @@
+require 'pry'
+
 class Grains
 
-  def self.square(square_number)
-    # 
+  def self.square(square_num)
+    grains = 1
+    if square_num == 1
+      grains
+    else
+      (square_num - 1).times do
+        grains = grains * 2
+      end
+    end
+    grains
+  end
+
+  def self.total
+    squares = [1]
+    63.times do
+      squares << squares.last * 2
+    end
+    squares.inject(0) { |result, square| result + square }
   end
 end
