5ef56388ac744230293e7f7806983dd5a3f5ea07
Brant Wellman <BrantWellman@gmail.com>
2017-01-09 16:12:24 -0700

    Java Hello World

--- java/hello-world/src/main/java/HelloWorld.java
+++ java/hello-world/src/main/java/HelloWorld.java
@@ -1,2 +1,11 @@
 public class HelloWorld {
+  public static String hello(String name) {
+
+    if (name == null || name == "") {
+      return "Hello, World!";
+    } else {
+      return "Hello, " + name + "!";
+    }
+
+  }
 }
