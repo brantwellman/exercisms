a7604d2bc014803739eba9eea7114175ba560e38
Brant Wellman <BrantWellman@gmail.com>
2017-01-17 16:42:40 -0700

    First test passing - not hard-coded

--- java/etl/src/main/java/Etl.java
+++ java/etl/src/main/java/Etl.java
@@ -1,8 +1,17 @@
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
 
 public class Etl {
    public Map<String, Integer> transform(Map<Integer, List<String>> old) {
-      return null;
+      Map<String, Integer> dictionary = new HashMap<String, Integer>();
+      Set<Integer> keys = old.keySet();
+
+      for (Integer key : keys) {
+        dictionary.put(old.get(key).get(0).toLowerCase(), key);
+      }
+
+      return dictionary;
    }
 }
