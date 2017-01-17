165027a9a26d746e00326cb46b8108240f33095d
Brant Wellman <BrantWellman@gmail.com>
2017-01-17 16:55:24 -0700

    First test passing - iterate with forEach thru Map

--- java/etl/src/main/java/Etl.java
+++ java/etl/src/main/java/Etl.java
@@ -6,11 +6,8 @@ import java.util.Set;
 public class Etl {
    public Map<String, Integer> transform(Map<Integer, List<String>> old) {
       Map<String, Integer> dictionary = new HashMap<String, Integer>();
-      Set<Integer> keys = old.keySet();
 
-      for (Integer key : keys) {
-        dictionary.put(old.get(key).get(0).toLowerCase(), key);
-      }
+      old.forEach( (k, v) -> dictionary.put(v.get(0).toLowerCase(), k));
 
       return dictionary;
    }
