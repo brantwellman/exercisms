ed2f121770cbd8dcc122abe007e7ec7d4c236a77
Brant Wellman <BrantWellman@gmail.com>
2017-01-18 14:07:07 -0700

    Java - DNA - hard code first 2 tests

--- java/nucleotide-count/src/main/java/DNA.java
+++ java/nucleotide-count/src/main/java/DNA.java
@@ -1,2 +1,23 @@
+import java.util.Map;
+import java.util.HashMap;
+
 public class DNA {
+  private String dnaString;
+
+  public DNA(String dnaString) {
+    this.dnaString = dnaString;
+  }
+
+  public int count(Character nucleotide) {
+    return 0;
+  }
+
+  public Map<Character, Integer> nucleotideCounts() {
+    Map<Character, Integer> map = new HashMap();
+    map.put('A',0);
+    map.put('C',0);
+    map.put('G',0);
+    map.put('T',0);
+    return map;
+  }
 }
