f7674bcc5bdee4751a997059aab34787f16d439d
Brant Wellman <BrantWellman@gmail.com>
2017-01-19 12:14:25 -0700

    Java - DNA - refactor

--- java/nucleotide-count/src/main/java/DNA.java
+++ java/nucleotide-count/src/main/java/DNA.java
@@ -3,40 +3,46 @@ import java.util.HashMap;
 
 public class DNA {
   private String dnaString;
+  private static String validSymbols = "ACGT";
+
 
   public DNA(String dnaString) {
     this.dnaString = dnaString;
   }
 
   public int count(Character nucleotide) {
-    String validSymbols = "GACT";
     if (validSymbols.indexOf(nucleotide) == -1) {
       throw new IllegalArgumentException("That is not a valid nucleotide.");
     }
 
     int counter = 0;
-    for (int i=0; i < dnaString.length(); i++) {
-      char c = dnaString.charAt(i);
+    for (char c : dnaString.toCharArray()) {
       if (nucleotide.equals(c)) {
         counter++;
       }
     }
+
     return counter;
   }
 
   public Map<Character, Integer> nucleotideCounts() {
-    Map<Character, Integer> counts = new HashMap();
-    counts.put('A',0);
-    counts.put('C',0);
-    counts.put('G',0);
-    counts.put('T',0);
+    Map<Character, Integer> counts = getNucleotideCounter();
 
-    for (int i=0; i < dnaString.length(); i++) {
-      char c = dnaString.charAt(i);
+    for (char c : dnaString.toCharArray()) {
       int count = counts.get(c);
       counts.put(c, count + 1);
     }
 
     return counts;
+  }
+
+  private Map<Character, Integer> getNucleotideCounter() {
+    Map<Character, Integer> initialCounter = new HashMap();
+    
+    for (char c : validSymbols.toCharArray()) {
+      initialCounter.put(c, 0);
+    }
+
+    return initialCounter;
   }
 }
