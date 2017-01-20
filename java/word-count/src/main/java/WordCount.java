e202f95ec3d4d1db5f87d984fe7ed68e967299c9
Brant Wellman <BrantWellman@gmail.com>
2017-01-20 11:55:18 -0700

    Java - WordCount - First test hardcoded, passing

--- java/word-count/src/main/java/WordCount.java
+++ java/word-count/src/main/java/WordCount.java
@@ -1,2 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class WordCount {
+
+  public Map<String, Integer> phrase(String words) {
+    Map<String, Integer> wordCount = new HashMap<String, Integer>();
+    wordCount.put("word", 1);
+    return wordCount;
+  }
 }
