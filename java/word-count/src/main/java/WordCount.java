afabbae3b0cf3873fc79a46f6d2758fc5578ad7f
Brant Wellman <BrantWellman@gmail.com>
2017-01-23 14:20:35 -0700

    Java - wordcount - all tests passing

--- java/word-count/src/main/java/WordCount.java
+++ java/word-count/src/main/java/WordCount.java
@@ -1,11 +1,29 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WordCount {
 
-  public Map<String, Integer> phrase(String words) {
+  public Map<String, Integer> phrase(String phraseWords) {
     Map<String, Integer> wordCount = new HashMap<String, Integer>();
-    wordCount.put("word", 1);
+
+    String sanitized = phraseWords.replaceAll("[^a-zA-Z0-9\\s]", "").replaceAll("\\s+", " ").toLowerCase();
+    String[] words = sanitized.split(" ");
+    int i = 0;
+    while (i < words.length) {
+      if (wordCount.containsKey(words[i])) {
+          int count = wordCount.get(words[i]);
+          count++;
+          wordCount.put(words[i], count);
+      } else {
+          wordCount.put(words[i], 1);
+      }
+      i++;
+    }
+
     return wordCount;
   }
+
 }
